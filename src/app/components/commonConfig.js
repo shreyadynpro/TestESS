ac662ac9bd2a632ddd6c570fd3ff7abbfcc03c0d
Mahesh K <mahesh.bhalchandra@dynpro.com>
2024-08-14 18:29:37 +0530

    API Server Host URL Changes

--- src/app/components/commonConfig.js
+++ src/app/components/commonConfig.js
@@ -1,5 +1,5 @@
 const serverIp = '44.199.193.239';
-const baseURL = 'https://hcaapi.kairosrp.com/api';
+const baseURL = 'http://192.168.1.239/api';
 // const baseURL = 'http://127.0.0.1:8000/api';
 
 const commonConfig = {
