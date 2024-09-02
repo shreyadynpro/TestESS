65ea0bf9481e2a5a868bf537ee34aa092496668f
jahnavi-dynpro <jahnavi.n@dynpro.in>
2024-09-02 13:56:17 +0530

    added the user name in the sidenav

--- src/app/components/Sidenav.jsx
+++ src/app/components/Sidenav.jsx
@@ -1,5 +1,5 @@
 import { styled } from '@mui/system';
-import { Fragment, memo, useState, useEffect } from 'react';
+import { Fragment, memo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Scrollbar from 'react-perfect-scrollbar';
 import { Box, Typography, Collapse } from '@mui/material';
@@ -8,6 +8,7 @@ import SecurityIcon from '@mui/icons-material/Security';
 import LockIcon from '@mui/icons-material/Lock';
 import PersonIcon from '@mui/icons-material/Person';
 import GroupsIcon from '@mui/icons-material/Groups';
+import BadgeIcon from '@mui/icons-material/Badge';
 import SummarizeIcon from '@mui/icons-material/Summarize';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import EventNoteIcon from '@mui/icons-material/EventNote';
@@ -21,10 +22,11 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { useSelector } from 'react-redux';
 import commonRoutes from './commonRoutes';
 
-const StyledScrollBar = styled(Scrollbar)(() => ({
+const StyledScrollBar = styled(Scrollbar)(({ theme }) => ({
   paddingLeft: '0.1rem',
   paddingRight: '0.1rem',
   position: 'relative',
+  backgroundColor: '#fafafa',
 }));
 
 const SideNavMobile = styled('div')(({ theme }) => ({
@@ -39,25 +41,32 @@ const SideNavMobile = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('lg')]: { display: 'none' },
 }));
 
-const MenuItem = styled(Box)(({ theme }) => ({
+const MenuItem = styled(({ isProfile, ...rest }) => <Box {...rest} />)(({ theme, isProfile }) => ({
   display: 'flex',
   alignItems: 'center',
   padding: '10px 20px',
   cursor: 'pointer',
+  fontWeight: isProfile ? 'bold' : 'normal',
   '&:hover': {
     backgroundColor: theme.palette.action.hover,
   },
 }));
+
+const MenuLabel = styled(({ isProfile, ...rest }) => <Typography {...rest} />)(({ theme, isProfile, fontSize, fontWeight, color }) => ({
+  fontWeight: isProfile ? 'bold' : fontWeight || 'medium',
+  fontStyle: isProfile ? 'italic' : 'normal',
+  fontSize: fontSize || '16px',
+  color: isProfile ? '#00246b' : color || theme.palette.text.primary, 
+}));
+
+
 
 const MenuIcon = styled(Box)(({ theme }) => ({
   fontSize: '24px',
   marginRight: '16px',
 }));
 
-const MenuLabel = styled(Typography)(({ theme }) => ({
-  fontSize: '16px',
-  fontWeight: 'medium',
-}));
+
 
 const MenuArrow = styled(Box)(({ theme }) => ({
   marginLeft: 'auto',
@@ -69,144 +78,155 @@ const SubMenuItem = styled(MenuItem)(({ theme }) => ({
   backgroundColor: theme.palette.action.hover,
 }));
 
-const allMenuItems = [
-  {
-    title: 'Group Master',
-    path: '/group-master',
-    icon: <GroupIcon />,
-    permissionKey: 'groups',
-  },
-  { title: 'Roles', path: '/role', icon: <SecurityIcon />, permissionKey: 'roles' },
-  {
-    title: 'User Access Control',
-    path: '/groups',
-    icon: <LockIcon />,
-    permissionKey: 'users',
-  },
-  { title: 'Users', path: '/splitUsers', icon: <PersonIcon />, permissionKey: 'users' },
-  {
-    title: 'Employees',
-    path: '/employee-master',
-    icon: <GroupsIcon />,
-    permissionKey: 'employees',
-  },
-  {
-    title: 'Power BI',
-    path: '/reports',
-    icon: <SummarizeIcon />,
-    permissionKey: 'reports',
-    subMenu: [
-      {
-        title: 'Demo',
-        path: commonRoutes.reports.reportsGet.replace(
-          ':id',
-          '0d6cf1ab-d1c5-4f14-af60-16a1c83d67b0'
-        ),
-        permissionKey: 'reports',
-      },
-      {
-        title: 'Employee Report',
-        path: commonRoutes.reports.reportsGet.replace(
-          ':id',
-          '879238e6-8b3b-485f-9c16-001f204fd47c'
-        ),
-        permissionKey: 'reports',
-      },
-    ],
-  },
-  {
-    title: 'Zoho',
-    path: '/zohoreports',
-    icon: <SummarizeIcon />,
-    permissionKey: 'reports',
-    subMenu: [
-      {
-        title: 'Demo',
-        path: commonRoutes.Zohoreports.reportsZohoGet.replace(':id', '377745000000002340'),
-        permissionKey: 'reports',
-      },
-    ],
-  },
-  {
-    title: 'Leave',
-    path: '/leave',
-    icon: <EventNoteIcon />,
-    permissionKey: 'leaves',
-    subMenu: [
-      { title: 'Leave Type Master', path: '/leave/sick', permissionKey: 'leaves' },
-      { title: 'Leave Apply', path: '/leave/casual', permissionKey: 'leaves' },
-      { title: 'Grant Leave', path: '/leave/grant', permissionKey: 'leaves' },
-      { title: 'Leave Balance', path: '/leave/balance', permissionKey: 'leaves' },
-      {
-        title: 'Leave Approval Tracking',
-        path: '/leave/approval-tracking',
-        permissionKey: 'leaves',
-      },
-      { title: 'Leave Calendar', path: '/leave/calendar', permissionKey: 'leaves' },
-      {
-        title: 'Manage Holiday Calendar',
-        path: '/leave/manage-calendar',
-        permissionKey: 'leaves',
-      },
-      {
-        title: 'Holiday Calendar',
-        path: '/leave/holiday-calendar',
-        permissionKey: 'leaves',
-      },
-    ],
-  },
-  {
-    title: 'Salary',
-    path: '/salary',
-    icon: <MonetizationOnIcon />,
-    permissionKey: 'salary',
-    subMenu: [
-      { title: 'Payslips', path: '/salary/payslips', permissionKey: 'salary' },
-      {
-        title: 'IT Statements',
-        path: '/salary/it-statements',
-        permissionKey: 'salary',
-      },
-      {
-        title: 'IT Declaration',
-        path: '/salary/it-declaration',
-        permissionKey: 'salary',
-      },
-      {
-        title: 'Loans & Advances',
-        path: '/salary/loans-advances',
-        permissionKey: 'salary',
-      },
-      {
-        title: 'Reimbursement',
-        path: '/salary/reimbursement',
-        permissionKey: 'salary',
-      },
-      { title: 'Salary Revision', path: '/salary/revision', permissionKey: 'salary' },
-    ],
-  },
-  { title: 'Documents', path: '/documents', icon: <DescriptionIcon />, permissionKey: 'documents' },
-  {
-    title: 'Attendance',
-    path: '/attendance',
-    icon: <CalendarMonthIcon />,
-    permissionKey: 'attendance',
-    subMenu: [
-      { title: 'Daily', path: '/attendance/daily', permissionKey: 'attendanceDaily' },
-      { title: 'Monthly', path: '/attendance/monthly', permissionKey: 'attendanceMonthly' },
-    ],
-  },
-  { title: 'Activities', path: '/activities', icon: <CampaignIcon />, permissionKey: 'activities' },
-  { title: 'Help', path: '/help', icon: <InfoIcon />, permissionKey: 'help' },
-];
-
 const Sidenav = () => {
   const [openMenu, setOpenMenu] = useState({});
   const navigate = useNavigate();
+  const user = useSelector((state) => state.userDetails?.user);
 
   const { uaPermissions } = useSelector((state) => ({
     uaPermissions: state.userAccessPermissions.userPermissions,
   }));
+
+  const allMenuItems = [
+    { 
+      title: `${user?.first_name || ''} ${user?.last_name || ''},`, 
+      path: '/Profile', 
+      fontSize: '20px',
+      fontWeight: 'bold',
+      color:'#00246b',
+      isProfile: true, 
+      // icon: <BadgeIcon />,
+      permissionKey: 'roles'
+    },
+    {
+      title: 'Group Master',
+      path: '/group-master',
+      icon: <GroupIcon />,
+      permissionKey: 'groups',
+    },
+    { title: 'Roles', path: '/role', icon: <SecurityIcon />, permissionKey: 'roles' },
+    {
+      title: 'User Access Control',
+      path: '/groups',
+      icon: <LockIcon />,
+      permissionKey: 'users',
+    },
+    { title: 'Users', path: '/splitUsers', icon: <PersonIcon />, permissionKey: 'users' },
+    {
+      title: 'Employees',
+      path: '/employee-master',
+      icon: <GroupsIcon />,
+      permissionKey: 'employees',
+    },
+    {
+      title: 'Power BI',
+      path: '/reports',
+      icon: <SummarizeIcon />,
+      permissionKey: 'reports',
+      subMenu: [
+        {
+          title: 'Demo',
+          path: commonRoutes.reports.reportsGet.replace(
+            ':id',
+            '0d6cf1ab-d1c5-4f14-af60-16a1c83d67b0'
+          ),
+          permissionKey: 'reports',
+        },
+        {
+          title: 'Employee Report',
+          path: commonRoutes.reports.reportsGet.replace(
+            ':id',
+            '879238e6-8b3b-485f-9c16-001f204fd47c'
+          ),
+          permissionKey: 'reports',
+        },
+      ],
+    },
+    {
+      title: 'Zoho',
+      path: '/zohoreports',
+      icon: <SummarizeIcon />,
+      permissionKey: 'reports',
+      subMenu: [
+        {
+          title: 'Demo',
+          path: commonRoutes.Zohoreports.reportsZohoGet.replace(':id', '377745000000002340'),
+          permissionKey: 'reports',
+        },
+      ],
+    },
+    {
+      title: 'Leave',
+      path: '/leave',
+      icon: <EventNoteIcon />,
+      permissionKey: 'leaves',
+      subMenu: [
+        { title: 'Leave Type Master', path: '/leave/sick', permissionKey: 'leaves' },
+        { title: 'Leave Apply', path: '/leave/casual', permissionKey: 'leaves' },
+        { title: 'Grant Leave', path: '/leave/grant', permissionKey: 'leaves' },
+        { title: 'Leave Balance', path: '/leave/balance', permissionKey: 'leaves' },
+        {
+          title: 'Leave Approval Tracking',
+          path: '/leave/approval-tracking',
+          permissionKey: 'leaves',
+        },
+        { title: 'Leave Calendar', path: '/leave/calendar', permissionKey: 'leaves' },
+        {
+          title: 'Manage Holiday Calendar',
+          path: '/leave/manage-calendar',
+          permissionKey: 'leaves',
+        },
+        {
+          title: 'Holiday Calendar',
+          path: '/leave/holiday-calendar',
+          permissionKey: 'leaves',
+        },
+      ],
+    },
+    {
+      title: 'Salary',
+      path: '/salary',
+      icon: <MonetizationOnIcon />,
+      permissionKey: 'salary',
+      subMenu: [
+        { title: 'Payslips', path: '/salary/payslips', permissionKey: 'salary' },
+        {
+          title: 'IT Statements',
+          path: '/salary/it-statements',
+          permissionKey: 'salary',
+        },
+        {
+          title: 'IT Declaration',
+          path: '/salary/it-declaration',
+          permissionKey: 'salary',
+        },
+        {
+          title: 'Loans & Advances',
+          path: '/salary/loans-advances',
+          permissionKey: 'salary',
+        },
+        {
+          title: 'Reimbursement',
+          path: '/salary/reimbursement',
+          permissionKey: 'salary',
+        },
+        { title: 'Salary Revision', path: '/salary/revision', permissionKey: 'salary' },
+      ],
+    },
+    { title: 'Documents', path: '/documents', icon: <DescriptionIcon />, permissionKey: 'documents' },
+    {
+      title: 'Attendance',
+      path: '/attendance',
+      icon: <CalendarMonthIcon />,
+      permissionKey: 'attendance',
+      subMenu: [
+        { title: 'Daily', path: '/attendance/daily', permissionKey: 'attendanceDaily' },
+        { title: 'Monthly', path: '/attendance/monthly', permissionKey: 'attendanceMonthly' },
+      ],
+    },
+    { title: 'Activities', path: '/activities', icon: <CampaignIcon />, permissionKey: 'activities' },
+    { title: 'Help', path: '/help', icon: <InfoIcon />, permissionKey: 'help' },
+  ];
 
   const filterMenuItems = (items) => {
     return items
@@ -233,35 +253,48 @@ const Sidenav = () => {
         [path]: false,
       }));
     }
-  };
-
-  const renderMenuItems = (items, level = 0) => {
-    return items.map((item) => (
-      <Fragment key={item.title}>
-        <MenuItem
-          onClick={() => handleMenuItemClick(item.path, item.subMenu)}
-          style={{ paddingLeft: `${20 + level * 20}px` }}
-        >
-          <MenuIcon>{item.icon}</MenuIcon>
-          <MenuLabel>{item.title}</MenuLabel>
-          {item.subMenu && (
-            <MenuArrow>{openMenu[item.path] ? <ExpandLessIcon /> : <ExpandMoreIcon />}</MenuArrow>
-          )}
-        </MenuItem>
-        {item.subMenu && (
-          <Collapse in={openMenu[item.path]}>{renderMenuItems(item.subMenu, level + 1)}</Collapse>
-        )}
-      </Fragment>
-    ));
+    
   };
 
   return (
     <Fragment>
       <StyledScrollBar options={{ suppressScrollX: true }}>
-        {renderMenuItems(menuItems)}
+        {menuItems.map((item, index) => (
+          <Fragment key={index}>
+            <MenuItem
+              onClick={() => handleMenuItemClick(item.path, !!item.subMenu)}
+              isProfile={item.isProfile}
+            >
+              <MenuIcon>{item.icon}</MenuIcon>
+              <MenuLabel
+                isProfile={item.isProfile}
+                fontSize={item.fontSize}
+                fontWeight={item.fontWeight}
+              >
+                {item.title}
+              </MenuLabel>
+              {item.subMenu && (
+                <MenuArrow>
+                  {openMenu[item.path] ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                </MenuArrow>
+              )}
+            </MenuItem>
+            {item.subMenu && (
+              <Collapse in={openMenu[item.path]} timeout="auto" unmountOnExit>
+                {item.subMenu.map((subItem, subIndex) => (
+                  <SubMenuItem
+                    key={`${index}-${subIndex}`}
+                    onClick={() => handleMenuItemClick(subItem.path, !!subItem.subMenu)}
+                  >
+                    <MenuLabel>{subItem.title}</MenuLabel>
+                  </SubMenuItem>
+                ))}
+              </Collapse>
+            )}
+          </Fragment>
+        ))}
       </StyledScrollBar>
-
-      <SideNavMobile onClick={() => setOpenMenu({})} />
+      <SideNavMobile />
     </Fragment>
   );
 };
