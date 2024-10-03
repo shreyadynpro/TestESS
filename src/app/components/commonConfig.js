4929c18abb7a26ae47234836f6263d0b9e60dbeb
MaheshkDynpro <165248894+MaheshkDynpro@users.noreply.github.com>
2024-10-03 11:53:37 +0530

    Update commonConfig.js

--- src/app/components/commonConfig.js
+++ src/app/components/commonConfig.js
@@ -1,5 +1,5 @@
 const serverIp = '44.199.193.239';
-const baseURL = ''https://ess.dynprocloud.com:8443/api';
+const baseURL = 'https://ess.dynprocloud.com:8443/api';
 // const baseURL = 'http://127.0.0.1:8000/api';
 
 const commonConfig = {
